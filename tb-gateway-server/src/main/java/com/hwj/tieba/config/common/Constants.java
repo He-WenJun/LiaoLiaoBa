9d1a82b98396df355509414512ecd7dbabcbcc0e
zhanmou <3047370075@qq.com>
2020-04-11 00:09:23 +0800

    submit_3

--- tb-gateway-server/src/main/java/com/hwj/tieba/config/common/Constants.java
+++ tb-gateway-server/src/main/java/com/hwj/tieba/config/common/Constants.java
@@ -2,7 +2,7 @@ package com.hwj.tieba.config.common;
 
 public class Constants {
     private Constants(){}
-    /**登录token的key*/
+    /**登录token的key */
     public static final String LOGIN_NAME_KEY = "LoginNameKey";
     /**登录token*/
     public static final String LOGIN_TOKEN = "LoginToken";
